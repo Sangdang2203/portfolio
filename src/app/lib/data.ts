ab49c446bce70b8dee9748e558da61b579a9c15d
Đặng Văn Sang <122693847+Sangdang2203@users.noreply.github.com>
2025-04-25 18:25:15 +0700

    refactored code

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,48 +1,4 @@
-export interface Experience {
-  id: number;
-  company: string;
-  duration: string;
-  role: string;
-  address: string;
-  describe: string;
-  contributions: { id: number; describe: string }[];
-  deployed_link?: string;
-}
-
-export interface NavLink {
-  key: string;
-  label: string;
-  href: string;
-}
-export interface Project {
-  id: number;
-  title: string;
-  duration: string;
-  teamSize: number;
-  describe: string;
-  githubLinks: {
-    id: number;
-    name: string;
-    link: string;
-  }[];
-  deployedLinks: { id: number; link: string }[];
-  technologies: { id: number; name: string; description: string }[];
-  contributions: string;
-  // contributions: { id: number; describe: string }[];
-}
-
-export interface Skill {
-  id: number;
-  title: string;
-  description: string;
-}
-
-export interface Certificate {
-  id: number;
-  title: string;
-  describe: string;
-  link: string;
-}
+import { Certificate, Experience, NavLink, Project, Skill } from "./types";
 
 export const navLinks: NavLink[] = [
   {
